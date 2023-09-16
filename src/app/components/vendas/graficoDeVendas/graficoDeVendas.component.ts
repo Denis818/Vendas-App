ebaab8a62de9e85d67aa33be1e03b729633438dd
Denis Developer <denispereiradosreis@gmail.com>
2023-09-16 12:41:41 -0300

    adicionando spinner

--- src/app/components/vendas/graficoDeVendas/graficoDeVendas.component.ts
+++ src/app/components/vendas/graficoDeVendas/graficoDeVendas.component.ts
@@ -4,6 +4,7 @@ import { ToastrService } from 'ngx-toastr';
 import 'chartjs-plugin-datalabels';
 import { VendaService } from '../../../services/venda.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { forkJoin } from 'rxjs';
 
 
 @Component({
@@ -29,28 +30,38 @@ export class GraficoDeVendasComponent implements OnInit {
           },
           color: '#708090'
         }
+      },
+      tooltip: {
+        callbacks: {
+          label: this.graficoFormatPreco
+        }
       }
     }
   };
 
-  public graficoVendasPorDiaLabel = [];
-  public graficoVendasPorDiaDados = [
-    {
-      data: [],
-      backgroundColor: ['#d22030', '#51bbcb', '#606b6d', '#93b9c6','#f2d249', '#ccc5a8', '#26867a' ]
-    }
-  ];
-
-
-  chartOptionsProdutosResumo: ChartOptions = {
+  public chartOptionsProdutosResumo: ChartOptions = {
     responsive: true,
     scales: {
       y: {
         beginAtZero: true
       }
+    },
+    plugins:{
+      tooltip: {
+        callbacks: {
+          label: this.graficoFormatPreco
+        }
+      }
     }
   };
 
+  public graficoVendasPorDiaLabel: any = [];
+  public graficoVendasPorDiaDados = [
+    {
+      data: [] as any,
+      backgroundColor: ['#d22030', '#51bbcb', '#606b6d', '#93b9c6', '#f2d249', '#ccc5a8', '#26867a']
+    }
+  ];
 
   public produtosResumoLabels: any = [];
   public produtosResumoDatasets = [
@@ -64,17 +75,6 @@ export class GraficoDeVendasComponent implements OnInit {
     }
   ];
 
-  public chart3Labels = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'];
-  public chart3Datasets = [
-    {
-      data: [65, 59, 80, 81, 56, 55],
-      label: 'Vendas',
-      borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 2,
-      fill: false
-    }
-  ];
-
   constructor(private vendaServices: VendaService,
     private spinner: NgxSpinnerService,
     private toastr: ToastrService) {
@@ -82,44 +82,58 @@ export class GraficoDeVendasComponent implements OnInit {
 
   public ngOnInit() {
     this.spinner.show();
-    this.graficoDeVendas();
-    this.getResumoVendas();
+    this.carregarDados();
   }
 
-  public getResumoVendas() {
-    this.vendaServices.getSalesSummary().subscribe({
-      next: data => {
-        this.mediaDeVendaPorDia = data.mediaDeVendaPorDia;
-        this.produtoMaisVendido = data.produtoMaisVendido;
-        this.totalDoMaisVendido = data.totalDoMaisVendido;
-        this.totalDeTodasAsVendas = data.totalDeTodasAsVendas;
-        this.getProdutosResumoVendas(data.produtosResumo);
+  private carregarDados() {
+    forkJoin({
+      resumoVendas: this.vendaServices.getSalesSummary(),
+      graficoVendas: this.vendaServices.getGroupSalesDay()
 
+    }).subscribe({
+      next: results => {
+        this.processarResumoVendas(results.resumoVendas);
+        this.processarGraficoDeVendas(results.graficoVendas);
       },
       error: () => {
         this.spinner.hide();
-        this.toastr.error('Erro ao carregar resumo das Vendas.', 'Error');
+        this.toastr.error('Erro ao carregar os dados.', 'Error');
+      },
+      complete: () => {
+        this.spinner.hide();
       }
     });
   }
 
-  public getProdutosResumoVendas(produtosResumoVendas: any[]){
-    this.produtosResumoLabels = produtosResumoVendas.map(produto => produto.nome);
-    this.produtosResumoDatasets[0].data = produtosResumoVendas.map(produto => produto.totalDaVenda.toFixed(2));
-    this.produtosResumoDatasets[1].data = produtosResumoVendas.map(produto => produto.quantidadeTotalVendida);
+  private processarResumoVendas(produto: any) {
+    this.mediaDeVendaPorDia = produto.mediaDeVendaPorDia;
+    this.produtoMaisVendido = produto.produtoMaisVendido;
+    this.totalDoMaisVendido = produto.totalDoMaisVendido;
+    this.totalDeTodasAsVendas = produto.totalDeTodasAsVendas;
+    this.getProdutosResumoVendas(produto.produtosResumo);
   }
 
-  public graficoDeVendas(): void {
-    this.vendaServices.getGroupSalesDay().subscribe({
-      next: data => {
-        this.graficoVendasPorDiaLabel = data.map((venda: any) => venda.dia);
-        this.graficoVendasPorDiaDados[0].data = data.map((venda: any) => venda.total.toFixed(2));
-        this.spinner.hide();
-      },
-      error: () => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar gráfico de Vendas.', 'Error');
-      }
-    });
+  private processarGraficoDeVendas(produto: any[]) {
+    this.graficoVendasPorDiaLabel = produto
+    .map((venda: any) => venda.dia);
+
+    this.graficoVendasPorDiaDados[0].data = produto
+    .map((venda: any) => venda.total.toFixed(2));
+  }
+
+  public getProdutosResumoVendas(produtosResumoVendas: any[]) {
+    this.produtosResumoLabels = produtosResumoVendas
+    .map(produto => produto.nome);
+
+    this.produtosResumoDatasets[0].data = produtosResumoVendas
+    .map(produto => produto.totalDaVenda.toFixed(2));
+
+    this.produtosResumoDatasets[1].data = produtosResumoVendas
+    .map(produto => produto.quantidadeTotalVendida);
+  }
+
+  public graficoFormatPreco(context: any): string {
+    const value = context.parsed.y || context.parsed;
+    return `${context.label}: R$ ${value.toFixed(2)}`;
   }
 }
